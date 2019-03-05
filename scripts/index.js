86774e3c8ce86b840f379a8ed5241dc834e4b49b
Anthony DiRusso <ossurid@gmail.com>
2019-03-05 11:14:35 -0500

    adds leetspeak translator function

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,27 @@ const leetDictionary = {
     "E": 3,
     "G": 6,
     "I": 1,
-    "O": 0,
+    "O": "0", //stringing this one fixes a bug
     "S": 5,
     "T": 7,
 }
 
 function transl33t(originalString) {
     let result = '';
-    
+    //loop through each letter in original string
+    // if its in the dictionary, and if it is 
+    //put its value in the result 
+    //else just copy the letter into `result`
+    for (let i = 0; i < originalString.length; i++) {
+        let theLetter = originalString[i].toUpperCase();
+        console.log(theLetter);
+        console.log(leetDictionary[theLetter]);
+        if (leetDictionary[theLetter]) {
+            debugger;
+            result += leetDictionary[theLetter];
+        } else {
+            result += theLetter;
+        }
+    }
     return result;
 }
